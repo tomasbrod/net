4db5385c9b566d94985f3f8dd7786df19917f0f0
Tomas Brada <tomasbrod@azet.sk>
2014-08-02 21:47:11 +0200

    Added binding to pongpacket

--- PingPacket.pas
+++ PingPacket.pas
@@ -9,13 +9,23 @@ const pktype=1;
 type
  T =object(GeneralPacket.T)
   procedure Handle;
+  {
+  procedure Send;
+  procedure Create;
+  }
  end;
 
 IMPLEMENTATION
+{uses PongPacket;}
 
 procedure T.Handle;
  unimplemented;
+{var Pong:PongPacket.T;}
 begin
+ {
+ Pong.Create;
+ Pong.Repl;
+ }
  AbstractError;
 end;
 
