e1d5a8d391d6609d98a5a352d32c5d21d2412734
Tomas Brada <tomasbrod@azet.sk>
2014-08-06 19:56:06 +0200

    Implemented Hello.

--- Hello.pas
+++ Hello.pas
@@ -1,15 +1,5 @@
 unit Hello;
 
-(*
-   Hello packets are broadcasted ower LAN every cBroadcastPeriod ms. When 
-   Hello packet arrives, the sender is added to list of Peers. There is no 
-   more to it.
-   
-   Also, if the Hello packet is recieved in cHelloCooldown ms since last 
-   Hello with the same Fingerprint, it is discarded.
-      
-*)
-
 INTERFACE
 uses GeneralPacket
     ,Keys
@@ -20,31 +10,38 @@ const pktype=1;
 type
  tPacket =packed object(GeneralPacket.T)
   procedure Handle;
-  {
   procedure Send;
-  }
   procedure Create;
   private
   Fingerprint :keys.tFingerprint;
- end; {+1=?}
+ end;
 
 IMPLEMENTATION
 uses Peers
+    ,HiHello
     ;
 
 procedure tPacket.Handle;
+var repl:HiHello.tPacket;
 begin
- if Peers.TimeSinceLast(Hello.pktype) < cHelloCooldown
+ if Peers.TimeSinceLast < cHelloCooldown
   then exit; //Anti-DoS
- Peers.Add (Fingerprint);
+ Peers.Assoc (Fingerprint); {Associate sender's addres with fingerprint.}
+ Peers.Save (true);
+ repl.Create;
+ repl.Send;
 end;
 
 procedure tPacket.Create;
 var a:byte;
 begin
- inherited Create;
+ inherited Create(Hello.pktype);
  Fingerprint:=MyFingerPrint;
- self.pktype:=Hello.pktype;
+end;
+
+procedure tPacketSend;
+begin
+ Send(sizeof(self));
 end;
 
 END.
