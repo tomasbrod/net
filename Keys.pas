d1940ef6892dafa2a4d1180d70905e6c7c31f500
Tomas Brada <tomasbrod@azet.sk>
2014-08-30 18:15:33 +0200

    Keys Object impl.

--- Keys.pas
+++ Keys.pas
@@ -6,26 +6,49 @@ TYPE{s}
 
  tHash=object
   procedure Clear;
+   experimental;
   function isNil :boolean;
+   experimental;
   procedure ToString( var s :string );
+   experimental;
   procedure FromString( s :string );
+   experimental;
   private
   data :array [0..20] of byte;
  end;
+
  tFingerprint =tHash Deprecated;
 
 VAR
  MyFilgerPrint: tFingerPrint unimplemented;
  
-
 IMPLEMENTATION
+uses SysUtils;
 
 procedure tHash.Clear;
-begin abstracterror; end;
+var i:byte;
+begin for i:=low(data) to high(data) do data[i]:=0; end;
 
-function tHash.isNil :boolean;
+function  tHash.isNil :boolean;
+var i:byte;
 begin
- abstracterror;
+ isNil:=true;
+ for i:=low(data) to high(data) do if data[i]<>0 then isNil:=false;
+end;
+
+procedure tHash.ToString( var s :string );
+var i :byte;
+begin
+ s:='';
+ for i:=low(data) to high(data)
+  do s:=s+IntToHex(data[i],2);
+end;
+
+procedure tHash.FromString( s :string );
+var i :byte;
+begin
+ for i:=low(data) to high(data)
+  do data[i]:=StrToInt( 'x'+Copy(s, (2*i)-1, 2) );
 end;
 
 END.
