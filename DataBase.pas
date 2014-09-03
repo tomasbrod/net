74469405aa16f756727fb4a145538bf3835cda04
Tomas Brada <tomasbrod@azet.sk>
2014-09-03 17:29:09 +0200

    typo, units

--- DataBase.pas
+++ DataBase.pas
@@ -9,24 +9,26 @@ tRow = string[31];
 tField = string[15];
 
 var Prefix :string ='./data';
+{TODO: extract from parameters }
 
 const FieldExtension :string='dat';
 
-procedure Open( var F :FileName; const Table :tTable; const Row :tRow; const Field :tField );
+procedure Open( var F :File; const Table :tTable; const Row :tRow; const Field :tField );
 
 IMPLEMENTATION
+uses SysUtils;
 
-procedure Open( var F :FileName; const Table :tTable; const Row :tRow; const Field :tField );
+procedure Open( var F :File; const Table :tTable; const Row :tRow; const Field :tField );
 var path,name :tFileName;
 begin
  path:=
   Prefix + DirectorySeparator
   + Table + DirectorySeparator
-  + Row + DirectorySeparator
- name:=Field + ExtensionSeparator + Extension;
+  + Row + DirectorySeparator;
+ name:=Field + ExtensionSeparator + FieldExtension;
  {Create the file and directory if not exist}
  if not FileExists(path+name) then begin
-  if not DirecotryExists(path) then Force Directories(path);
+  if not DirectoryExists(path) then ForceDirectories(path);
   FileClose(FileCreate(path+name));
  end;
  { And Assign the handle. Note: caller should reset() the file. }
