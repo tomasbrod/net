592ed71c052fabf524123db00424fbef761a6b32
Tomas Brada <tomasbrod@azet.sk>
2014-09-22 19:48:05 +0200

    Transfer impl.

--- Transfer.pas
+++ Transfer.pas
@@ -15,41 +15,77 @@ CONST
 
 TYPE
 
+ tID=object(Keys.tHash)
+ end;
+ 
  tGet=object(Peers.tPacket)
-  procedure Create( id :Keys.tHash; part, count :Word );
+  procedure Create( id :tID; part, count :Word );
   procedure Handle;
   procedure Send;
   private
-  id :Keys.tHash;
+  id :tID;
   part :NetAddr.Word2;
   count :NetAddr.Word2;
  end;
  
  tDat=object(Peers.tPacket)
-  procedure Create( id :Keys.tHash; part :Word );
+  procedure Create( id :tID; part :Word );
   procedure Handle;
   procedure Send;
   private
-  id :Keys.tHash;
+  id :tID;
   part :NetAddr.Word2;
   total :NetAddr.Word2;
   PayLoad: array [1..cDataLength] of byte;
  end;
 
  tPcs=object(Peers.tPacket)
-  procedure Create( id :Keys.tHash; part :Word );
+  procedure Create( id :tID; part :Word );
   procedure Handle;
   procedure Send;
   private
-  id :Keys.tHash;
+  id :tID;
   part :NetAddr.Word2;
   total :NetAddr.Word2;
-  PayLoad: array [1..(cDataLength div sizeof(Keys.tHash))] of Keys.tHash;
+  PayLoad: array [1..(cDataLength div sizeof(tID))] of tID;
  end;
+
+var OnNoSrc :procedure( id :tHash );
+var OnRecv  :procedure( id :tHash );
 
 procedure SendFile( id: tID );
 
 procedure RecvFile( id :tID );
 
 procedure DoRetry;
+
+procedure Retry( id :tID );
+
+IMPLEMENTATION
+
+function IsPieced( id:tID ):boolean;
+ forward;
+procedure Retry( id :tID );
+ forward;
+
+procedure SendFile( id: tID );
+ var dat :tDat;
+ var pcs :tPcs;
+ {No tak bude na stacku nepotrebna premenna, svet sa nezruti.}
+ begin
+ if IsPieced( id ) then begin
+  pcs.Create( id, 0 );
+  pcs.Send;
+ end else begin
+  dat.Create( id, 0 );
+  dat.Send;
+ end;
+end;
+
+procedure RecvFile( id:tID );
+ begin
+ GlobalAddDownload( id );
+ AddSource( id, Peers.SelectedID );
+ Retry( ID );
+end;
 
