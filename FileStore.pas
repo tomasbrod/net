0ee6dadb621401b421970da33f54a10c44b84eca
Tomas Brada <tomasbrod@azet.sk>
2014-11-13 18:37:51 +0100

    Filestore interface.

---
+++ FileStore.pas
@@ -0,0 +1,17 @@
+unit FileStore;
+
+(* Uniform interface to all kinds of chk files *)
+
+INTERFACE
+
+Uses
+  Log
+ ,Keys
+ ;
+
+type tFID = object (Keys.tHash) end;
+
+IMPLEMENTATION
+Uses SimpleFiles;
+
+END.
