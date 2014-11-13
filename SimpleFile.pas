7beff2fca8fc936ec0b4fe46efc91cde1777f264
Tomas Brada <tomasbrod@azet.sk>
2014-11-13 18:38:21 +0100

    Impl SimpleFile.

--- SimpleFile.pas
+++ SimpleFile.pas
@@ -1,4 +1,4 @@
-unit SimpleFile;
+unit SimpleFiles;
 
 (*
 Managment of files in datastore
@@ -13,15 +13,16 @@ uses {srať to, dám všetky jednotky sem}
   ,dbf_common
   ,Classes
   ,Db
-  ,getopts
+  ,FileStore
+  ,Keys
   ;
 
 type tData = class (tDBFBlobStream)
  (* Read only stream with the data of the file *)
  public
  constructor Create( fid: tFID );
- destructor  Destroy; override;
  { for future reference
+ destructor  Destroy; override;
  function Read( var Buffer; Count: LongInt ) :LongInt; override;
  function Seek( const Offset: Int64; Origin: TSeekOrigin ):Int64; override
  }
@@ -30,16 +31,18 @@ type tData = class (tDBFBlobStream)
 procedure Add( fid: tFID; stream: tStream );
 (* Import data from the stream to database and run necessary checks. Throws 
 exception on error *)
+ type eExist= class (Exception) end; (* Attempt to add already-existing object *)
+ type eDataCheck= class (Exception) end; (* Hash not matching in the data *)
 
 function Exists( fid: tFID ) :boolean;
 
 procedure Delete( fid: tFID );
 
-function GetDataSet: tDbDataSet;
+{TODO: provide means of listing the files}
 
 IMPLEMENTATION
 
-(* The filestore
+(* The Store
 
 File based solution:
 
@@ -59,37 +62,82 @@ Database solution:
 - not using benefits of filesystem
 - avoids too many files and directories
 
+Mixed (small in db large in dir):
+
+- Not too many files
+- No big blob file
+
 *)
 
 {Currently we have the database solution here}
 
-type tFileTable = class( tDbDataSet )
+type tFileTable = class( DataBase.tDbDataSet )
  constructor Create; overload;
  end;
 
-var Filestore :tFileTable;
+var Store :tFileTable;
 
 constructor tFileTable.Create;
  begin
  inherited Create(nil);
- OpenMode:=omAutoCreate;
  FieldDefs.Add( {0} 'key',    ftString,  40, True  );
  FieldDefs.Add( {1} 'data',    ftBlob,   0, False  );
  with IndexDefs.Add do begin Name:='key'; Expression:=Name; Options:=[ixPrimary, ixUnique, ixCaseInsensitive]; end;
  Open ('simplefiles');
 end;
 
-function OpenStream( fid: tFID; mode: db.tBlobStreamMode ) : Classes.tStream;
+constructor tData.Create( fid: tFID );
+ var SuccessLocate:boolean;
+ begin
+ SuccessLocate:= Store.Locate( 'key', String(tHash(fid)), [] );
+ if not SuccessLocate then raise DataBase.eSearch.Create;
+ inherited Create( Store.Fields[1] );
+end;
+
+procedure Add( fid: tFID; stream: tStream );
+ var SuccessLocate:boolean;
  var idstr:string;
+ var trg:tDbfBlobStream;
+ var DataHash :tHash;
  begin
  idstr:= String(tHash(fid));
- if not Filestore.Locate( 'key', idstr, [] ) then begin
-  if mode=db.bmWrite then begin
-   Filestore.Append;
-   Filestore.Field[0].AsString:= idstr;
-  end else raise DataBase.eSearch.Create
- end;
- OpenStream:= CreateBlobStream(Filestore.Fields[1], mode);
+ SuccessLocate:= Store.Locate( 'key', idstr, [] );
+ if SuccessLocate then raise eExist.Create('');
+ Store.Append;
+ Store.Fields[0].AsString:=idstr;
+ trg:=tDbfBlobStream.Create( Store.Fields[1] );
+ try
+  trg.size:=0;
+  trg.CopyFrom( stream, 0 );
+  trg.Position:=0; {start from begining}
+  DataHash.Compute( trg ); {compute hash}
+  if DataHash = tHash(fid) then begin
+   trg.Commit;
+   Store.Post;
+  end else begin
+   trg.Cancel;
+   raise eDataCheck.Create('');
+  end;
+ finally trg.Free; end;
+end;
+  
+function Exists( fid: tFID ) :boolean;
+ var SuccessLocate:boolean;
+ begin
+ SuccessLocate:= Store.Locate( 'key', String(tHash(fid)), [] );
+ Exists:=SuccessLocate;
 end;
  
+procedure Delete( fid: tFID );
+ var SuccessLocate:boolean;
+ begin
+ SuccessLocate:= Store.Locate( 'key', String(tHash(fid)), [] );
+ if not SuccessLocate then raise DataBase.eSearch.Create;
+ Store.Delete;
+end;
 
+INITIALIZATION
+ Store:=tFileTable.Create;
+FINALIZATION
+ Store.Destroy;
+END.
