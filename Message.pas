20f86a3b5614adae721632ee1839cf013cbe1a23
Tomáš Brada <tomasbrod@azet.sk>
2015-03-15 11:15:36 +0100

    more design

--- Message.pas
+++ Message.pas
@@ -2,22 +2,36 @@ unit Message;
 
 INTERFACE
 
+procedure Send( data: tFID; recipient: tPID; aTags: byte{?} );
+ {tags are composed of (3)file keywords, (2)recipient tags and (1)additional tags}
+
+ { every hop the tags are replaced with tags of the recipient if known, else unmodified }
+
 {packets}
-type tMsgSend=object(tPacket)
+const cSend=10;
+type  tSend=object(tPacket)
  recipient: tPID;
  MsgBody: tFID;
- tags :array [1..64] of tKeyWord;
- end;
-type tMsgConfirm=object(tPacket)
+ keywords :array [1..8] of tKeyWord;
+ tags :array [1..56] of tKeyWord;
+ end; {approx 300 Byte}
+const cConfirm=11;
+type  tConfirm=object(tPacket)
  recipient: tPID;
  MsgBody: tFID;
  confirm: tFID;
  end;
-type tMsgAck=object(tPacket)
+const cAck=12;
+type  tAckStatus= (magGood=4, magNeighb, magStatic, magTags, magFree
+                  ,magFail=18, magAlready, magNoInterest, magReject, magSpam, magBadSig
+                  ,magMax=31 );
+type  tAck=object(tPacket)
  recipient: tPID;
  MsgBody: tFID;
- status: (magOK, magAlready, magNoInterest, magGeneralReject);
+ status: tAckStatus;
  end;
+
+const cStorage=10; {identifier for StorageManager to not delete message files}
 
 IMPLEMENTATION
 uses Neighb,Transfer;
@@ -31,10 +45,8 @@ type tMsgMeta=object
  accessed: tDateTime;
  affil: (maNone=1, maStatic=8, maSender=10);
  tags :array [1..64] of tTag;
- {
- countHop: Word;
- countAlready: Word;
- }
+ statCount :array [tAckStatus] of LongWord;
+ statLast :array [tAckStatus] of tDateTime;
  end;
 
 END.
