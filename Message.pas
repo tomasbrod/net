2d63f024bf7dabad8499e13742c477e463a2de2a
Tomáš Brada <tomasbrod@azet.sk>
2015-03-18 20:46:19 +0100

    Work in msg.

--- Message.pas
+++ Message.pas
@@ -16,22 +16,35 @@ type  tSend=object(tPacket)
  MsgBody: tFID;
  keywords :array [1..8] of tKeyWord;
  tags :array [1..56] of tKeyWord;
+ procedure Handle( const from: NetAddr.t);
+ procedure Send( const rcpt: NetAddr.t );
  end; {approx 300 Byte}
 const cConfirm=11;
 type  tConfirm=object(tPacket)
  recipient: tPID;
  MsgBody: tFID;
  confirm: tFID;
+ procedure Handle( const from: NetAddr.t);
+ procedure Send( const rcpt: NetAddr.t );
  end;
+type  tDeliveryStatus= 
+                  (mdsGood=4, mdsNeighb, mdsStatic, mdsTags, mdsFree
+                  ,mdsFail=18, mdsAlready, mdsNoInterest, mdsReject, mdsSpam, mdsBadSig
+                  ,mdsMax=31 );
 const cAck=12;
-type  tAckStatus= (magGood=4, magNeighb, magStatic, magTags, magFree
-                  ,magFail=18, magAlready, magNoInterest, magReject, magSpam, magBadSig
-                  ,magMax=31 );
-type  tAck=object(tPacket)
- trid:byte; {0 if asynchronous >=1 if in response to tSend}
+type  tAck=object(tPacket) {to confirm reception of tSend}
+ trid:byte;
+ status: tDeliveryStatus;
+ procedure Handle( const from: NetAddr.t);
+ procedure Send( const rcpt: NetAddr.t );
+ end;
+const cProgress=13;
+type  tProgress=object(tPacket)
  recipient: tPID;
  MsgBody: tFID;
- status: tAckStatus;
+ status: tDeliveryStatus;
+ procedure Handle( const from: NetAddr.t);
+ procedure Send( const rcpt: NetAddr.t );
  end;
 
 const cStorage=10; {identifier for StorageManager to not delete message files}
@@ -43,18 +56,25 @@ uses SysUtils
     ,Storage
     ,NetAddr;
 
+const cRetryPeriod = 2000 /MSecsPerDay;
+const cRetryMax= 3;
+const cExpireUnafil= 7 /MinutesPerDay;
+const cExpireStatic= 5;
+const cExpireSender= 20;
+
 type tCHK=ContentHash.t;
 type tMsgMeta=packed object
  recipient: tPID;
  MsgBody: tFID;
  confirm: tFID;
+ missBody,missConfirm:boolean;
  from: tPID unimplemented;
  received: tDateTime;
  accessed: tDateTime;
  affil: (maNone=1, maStatic=8, maSender=10);
  tags :array [1..56] of tTag unimplemented;
- statCount :array [tAckStatus] of LongWord;
- statLast :array [tAckStatus] of tDateTime;
+ statCount :array [tDeliveryStatus] of LongWord;
+ statLast :array [tDeliveryStatus] of tDateTime;
  procedure Init;
  end;
 type tMsgMetaDs=object(tMsgMeta)
@@ -67,20 +87,29 @@ end;
 procedure tMsgMeta.Init;
  var i:byte;
  begin
+ missBody:=false;
+ missConfirm:=false;
  confirm.clear;
  received:=now;
  accessed:=now;
+ received:=now;
  FillChar(statCount,sizeof(statCount),0);
  FillChar(statLast,sizeof(statLast),0); {!}
  {msg.tags!}
 end;
 
-type tPending=object(tSend);
+type tPending=record 
  rcpt:netaddr.t;
  retry:byte;
+ case kind:byte of
+  cSend: (send:tSend);
+  cConfirm: (confirm:tConfirm);
 end;
 var Pending:array [1..15] of ^tPending;
 var LastPendingRetry:tDateTime;
+
+const StorePrefix='msg'+DirectorySeparator;
+var LastStoreScan:tDateTime;
  
 procedure Send( data: contenthash.t; recipient: tPID; array of tKeyWord );
  var msg:tMsgMetaDS;
@@ -115,21 +144,101 @@ procedure SendTo( var msg:tMsgMetaDs; const rcpt: netaddr.t );
  if px>high(pending) then error;
  New(pending[px]);
  pending[px]^.rcpt:=rcpt;
- with pending[px]^ do begin
+ pending[px]^.retry:=1;
+ pending[px]^.kind:=cSend;
+ with pending[px]^.send do begin
+  Create(cSend);
   trid:=px;
-  recipient: msg.recipient;
+  recipient:=msg.recipient;
   MsgBody:=msg.MsgBody;
   tags:=msg.tags;
+  Send(rcpt);
  end;
- pending[px]^.retry:=1;
- pending[px]^.Send(rcpt);
  LastPendingRetry:=now;
 end;
 
-{
+procedure RetryPending;
+ var px:byte;
+ begin
+ for px:=low(pending) to high(pending) do if assigned(pending[px]) then with pending[px]^ do begin
+  if retry>cRetryMax then begin
+   dispose(pending[px]);
+   pending[px]:=nil;
+   log;
+  end else begin
+   case kind of
+    cSend: send.Send(rcpt);
+    cConfirm: confirm.Send(rcpt);
+   end;
+   Inc(retry);
+  end;
+ end;
+ LastPendingRetry:=now;
+end;
+
+procedure ScanStore;
+ unimplemented;
+ begin
+ {load and call proc on each msg}
+end;
+
 procedure NotifyIdle;
- - retry pending
- - scan store and delete/resend
-}
+ begin
+ if now-LastPendingRetry > cRetryPeriod then RetryPending;
+ if now-LastStoreScan > cRetryPeriod then ScanStore;
+end;
+
+{handlers}
+procedure tSend.Handle( const from: NetAddr.t);
+ var msg:tMsgMetaDs;
+ var ack:tAck;
+ var done,total:longword;
+ begin 
+ try
+  msg.Load(MsgBody,recipient);
+ except
+  msg.Init;
+  msg.recipient:=recipient;
+  msg.MsgBody:=MsgBody;
+  {from:=}
+  {affil: (maNone=1, maStatic=8, maSender=10);}
+  missBody:=true;
+  msg.tags:=tags;
+ end;
+ msg.accessed:=Now;
+ {merge msg.tags and tags ... }
+ msg.Save;
+ ack.Create(cAck);
+ ack.trid:=trid;
+ if (msg.MissBody)and(msg.confirm.isNil) then begin
+  if msg.affil=maStatic then ack.status:=mdsStatic
+  {else if ... then ack.status:=mds...}
+  else ack.status:=mdsGood;
+  Transfer.RequestFile( from, MsgBody, cStorage );
+ end else maStatic: ack.status:=mdsAlready;
+ ack.Send(from);
+ if (not msg.confirm.isNil)and(not msg.MissConfirm) then SendConfirmTo(msg, from);
+end;
+
+procedure tConfirm.Handle( const from: NetAddr.t);
+ begin 
+ AbstractError;
+end;
+procedure tAck.Handle( const from: NetAddr.t);
+ begin 
+ AbstractError;
+end;
+
+
+{senders}
+procedure tSend.Send( const rcpt: NetAddr.t );
+ begin inherited Send( rcpt, sizeof(self); end;
+procedure tConfirm.Send( const rcpt: NetAddr.t );
+ begin inherited Send( rcpt, sizeof(self); end;
+procedure tAck.Send( const rcpt: NetAddr.t );
+ begin inherited Send( rcpt, sizeof(self); end;
+procedure tProgress.Send( const rcpt: NetAddr.t );
+ begin inherited Send( rcpt, sizeof(self); end;
+
 
 END.
