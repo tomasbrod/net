e42a13ba8cc8163cf07eff55072a7d340029b647
Tomáš Brada <tomasbrod@azet.sk>
2015-09-10 21:47:56 +0200

    Upload Manager mission statement.

---
+++ upmgr.pas
@@ -0,0 +1,10 @@
+UNIT UPMGR;
+{Upload Manager for brodnetd}
+{mission:
+ multiplex uploads thru TC connection
+ keep one TC connection per peer
+ mux other streams to TC
+}
+{should contain netwotk code?:
+ to cancel and prioritize multiplexed streams
+}
