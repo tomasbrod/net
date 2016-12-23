7edc39fb790a16b9f1d59fda7fb02787937b28bc
Tomáš Brada <tomasbrod@azet.sk>
2016-12-23 18:05:58 +0100

    ServerLoop refactoring: * improve structure * sort timer events by time

--- ServerLoop.pas
+++ ServerLoop.pas
@@ -48,9 +48,8 @@ var iNow:tTimeVal;
 var mNow:tMTime;
 function GetMTime:tMTime;
 
-procedure SetThreadName(name:pchar);
 function GetSocket(const rcpt:tNetAddr):tSocket;
-procedure SC(fn:pointer; retval:cint);
+procedure SetThreadName(name:pchar);
 
 function GetCfgStr(name:pchar):String;
 function GetCfgNum(name:pchar):Double;
@@ -102,18 +101,79 @@ procedure InitMTime;
   po_monotonicnanoclocks(GetMTimeOffsetSec,mil);
 end;
 
+(**** Timer / Sheduler ****)
 
-
-var s_inet:tSocket;
-
-type tPollTop=0..7;
-var pollArr: packed array [tPollTop] of tPollFd;
-type tFdHndDsc=record
- cb: tFDEventHandler; {proc+object}
+type tSheduled_ptr=^tSheduled; tSheduled=record
+  time:LongWord;
+  cb:tOnTimer;
+  next:tSheduled_ptr;
  end;
-var pollHnd: array [tPollTop] of tFdHndDsc;
-var pollTop: tPollTop;
+var ShedTop: ^tSheduled;
+var ShedUU: ^tSheduled;
+var PollTimeout:LongInt;
 
+procedure Shedule(timeout{ms}: LongWord; h:tOnTimer);
+  var c:^tSheduled;
+  var pp:^pointer;
+  begin
+  timeout:=timeout+mNow;
+  c:=ShedTop;
+  pp:=@ShedTop;
+  while assigned(c) do begin
+    if c^.time>timeout then break;
+    c:=c^.next;
+  end;
+  if Assigned(ShedUU) then begin
+    c:=ShedUU;
+    ShedUU:=c^.next;
+  end else New(c);
+  c^.time:=timeout;
+  c^.cb:=h;
+  c^.next:=pp^;
+  pp^:=c;
+end;
+
+procedure UnShedule(h:tOnTimer);
+  var cur:^tSheduled;
+  var pcur:^pointer;
+  begin
+  pcur:=@ShedTop;
+  cur:=pcur^;
+  while assigned(cur) do begin
+    if 0=CompareByte(cur^.cb,h,sizeof(h)) then begin
+      pcur^:=cur^.next; {unlink from main list}
+      cur^.next:=ShedUU; ShedUU:=cur; {link to unused}
+      cur:=pcur^;
+    end else begin
+      pcur:=@cur^.next;
+      cur:=pcur^;
+    end;
+  end;
+end;
+
+procedure Timer;
+  var cur:^tSheduled;
+  var delta:LongWord;
+  begin
+  {Sheduling}
+  mNow:=GetMTime;
+  cur:=ShedTop;
+  if assigned(cur) then begin
+    delta:=cur^.time-mNow;
+    if delta<=0 then begin
+      ShedTop:=cur^.next;
+      cur^.next:=ShedUU;
+      ShedUU:=cur;
+      cur^.cb;
+    end else begin
+      if delta<PollTimeout then PollTimeout:=delta;
+    end;
+  end;
+end;
+
+(*** Extended message delegation ***)
+
+var Buffer:array [1..4096] of byte;
 type tOpDesc=record
   hnd: tMessageHandler;
 end;
@@ -122,70 +182,6 @@ type tTrDesc=record
   hnd: tObjMessageHandler;
 end;
 var TrDesc: array [0..223] of tTrDesc;
-{will be dynamic size later :) }
-
-type tSheduled_ptr=^tSheduled; tSheduled=record
- left:LongWord;
- cb:tOnTimer;
- next:tSheduled_ptr;
- end;
-var ShedTop: ^tSheduled;
-var ShedUU: ^tSheduled;
-var LastShed: tMTime;
-var PollTimeout:LongInt;
-var Buffer:array [1..4096] of byte;
-var Config:tConfigSection;
-
-procedure SC(fn:pointer; retval:cint);
- begin
-  if retval < 0 then begin
-   raise eXception.Create(Format('Socket error %d operation %P',[SocketError,fn]));
-  end;
- end;
-
-procedure s_SetupInet;
- var bind_addr:tInetSockAddr;
- var turnon:cint;
- begin
-  with bind_addr do begin
-   sin_family:=AF_INET;
-   sin_port:=htons(trunc(GetCfgNum('port')));
-   sin_addr.s_addr:=0; {any}
-   s_inet:=fpSocket(sin_family,SOCK_DGRAM,IPPROTO_UDP);
-   SC(@fpSocket,s_inet);
-   turnon:=IP_PMTUDISC_DO;
-   SC(@fpsetsockopt,fpsetsockopt(s_inet, IPPROTO_IP, IP_MTU_DISCOVER, @turnon, sizeof(turnon)));
-  end;
-  SC(@fpBind,fpBind(s_inet,@bind_addr,sizeof(bind_addr)));
-  with PollArr[0] do begin
-   fd:=s_inet;
-   events:=pollIN;
-   revents:=0;
-  end;
- end;
-
-function GetSocket(const rcpt:tNetAddr):tSocket;
- begin
- result:=s_inet;
-end;
-procedure SendMessage(const data; len:word; const rcpt:tSockAddrL );
- begin
- {SC(@fpsendto,}fpsendto(s_inet,@data,len,0,@rcpt,sizeof(sockaddr_in)){)};
-end;
-procedure SendMessage(const data; len:word; const rcpt:tNetAddr );
- var sa:tSockAddrL;
- begin
- rcpt.ToSocket(sa);
- SendMessage(data,len,sa);
-end;
-procedure SendMessage(const data; len:word; const rcpt:tNetAddr; channel:word );
- begin
- SendMessage(data,len,rcpt);
- {todo: optimization??}
-end;
-
-
-(*** Extended message delegation ***)
 
 procedure SetupOpcode(OpCode:byte; handler:tMessageHandler);
   begin
@@ -209,9 +205,6 @@ procedure SetMsgTr(ID:Word; handler:tObjMessageHandler);
   assert(ID<=high(TrDesc));
   TrDesc[ID].hnd:=handler;
 end;
-
- var EventsCount:integer;
- var tp:tPollTop;
 
 procedure Reciever(var p:tPollFD);
   var pkLen:LongWord;
@@ -246,180 +239,14 @@ procedure Reciever(var p:tPollFD);
   end;
 end;
 
-
-{$IFDEF Linux}
-function prctl( option:cint; arg2,arg3,arg4,arg5:culong):cint; cdecl; external;
-const PR_SET_NAME=15;
-{$ENDIF}
-procedure SetThreadName(name:pchar);
-{$IFDEF Linux} begin prctl(PR_SET_NAME,culong(pchar(name)),0,0,0)
-{$ELSE}begin{$NOTE Custom thread mames not supported}
-{$ENDIF} end;
-
-procedure Timer;
- var cur:^tSheduled;
- var pcur:^pointer;
- var delta:LongWord;
- var tasks:word;
- begin
- {Sheduling}
- {gmagic with delta-time, increment mNow, ...}
- mNow:=GetMTime;
- delta:=mNow-LastShed;
- LastShed:=mNow;
- //writeln('DeltaTime: ',delta);
- {first tick all tasks}
- tasks:=0;
- cur:=ShedTop;
- while assigned(cur) do begin
-  if cur^.left<=delta then cur^.left:=0 else begin
-   dec(cur^.left,delta);
-   {also set next wake time}
-   if cur^.left<PollTimeout then PollTimeout:=cur^.left;
-  end;
-  {count tasks here}
-  inc(tasks);
-  cur:=cur^.next;
- end;
- {correct floating-point glitch}
- if pollTimeout=0 then pollTimeOut:=1;
- {run first runnable task}
- pcur:=@ShedTop;
- cur:=pcur^;
- while assigned(cur) do begin
-  if cur^.left=0 then begin
-   {unlink}
-   pcur^:=cur^.next;
-   {link to unused}
-   cur^.next:=ShedUU;
-   ShedUU:=cur;
-   {call}
-   cur^.cb;
-   {do rest later}
-   pollTimeout:=0;
-   break;
-  end;
-  pcur:=@cur^.next;
-  cur:=cur^.next;
- end;
-end;
-
-procedure Main;
- begin
- s_setupInet;
- ReleaseConfigSection(Config);
- while not terminated  do begin
-  PollTimeout:=5000;
-  Timer;
-  Flush(OUTPUT);
-  EventsCount:=fpPoll(@PollArr[0],PollTop,PollTimeout);
-  Timer;
-  if (eventscount=-1)and terminated then break;
-  if eventscount=-1 then break;  {fixme: print error}
-  if eventscount=0 then continue else begin
-   {INET socket}
-   Reciever(PollArr[0]);
-   {INET6...}
-   {Generic}
-   for tp:=1 to pollTop do if PollArr[tp].revents>0 then begin
-    PollHnd[tp].CB(PollArr[tp].rEvents);
-    PollArr[tp].revents:=0;
-   end;
-  end;
- end;
- if assigned(onTerminate) then onTerminate;
- CloseSocket(s_inet);
- if ReExec then fpExecv(paramstr(0),argv);
-end;
-
-procedure WatchFD(fd:tHandle; h:tFDEventHandler; e:LongWord);
- var opt: tPollTop;
-begin
- if assigned(h) then begin
-  PollHnd[pollTop].CB:=h;
-  PollArr[pollTop].fd:=fd;
-  PollArr[pollTop].events:=e;
-  PollArr[pollTop].revents:=0;
-  //writeln('Add watch ',pollTop,' on ',fd,' to ',IntToHex(qword(@h),8));
-  Inc(PollTop);
- end else for opt:=0 to high(opt) do if PollArr[opt].fd=fd then begin
-  if (pollTop-1)>opt then begin
-   PollArr[opt]:=PollArr[pollTop-1];
-   PollHnd[opt]:=PollHnd[pollTop-1];
-  end;
-  dec(pollTop);
-  PollArr[pollTop].fd:=-1;
-  PollArr[pollTop].events:=0;
-  PollArr[pollTop].revents:=0;
-  break;
- end;
-end;
-procedure WatchFD(fd:tHandle; h:tFDEventHandler);
-  begin
-  WatchFD(fd,h,POLLERR or POLLHUP or POLLIN or POLLPRI or 
-  POLLRDBAND or POLLRDNORM);
-end;
-procedure WatchFDRW(fd:tHandle; h:tFDEventHandler);
-  begin
-  WatchFD(fd,h,POLLERR or POLLHUP or POLLIN or POLLPRI or 
-  POLLRDBAND or POLLRDNORM or POLLOUT);
-end;
-
-procedure Shedule(timeout{ms}: LongWord; h:tOnTimer);
- var old:^tSheduled;
- begin
- old:=ShedTop;
- if Assigned(ShedUU) then begin
-  ShedTop:=ShedUU;
-  ShedUU:=ShedUU^.next;
- end else New(ShedTop);
- ShedTop^.Left:=timeout;
- ShedTop^.CB:=h;
- ShedTop^.Next:=old;
-end;
-
-procedure UnShedule(h:tOnTimer);
- var cur:^tSheduled;
- var pcur:^pointer;
- begin
- //if ShedTop=nil then AbstractError;
- pcur:=@ShedTop;
- cur:=pcur^;
- while assigned(cur) do begin
-  if 0=CompareByte(cur^.cb,h,sizeof(h)) then begin
-   pcur^:=cur^.next; {unlink from main list}
-   cur^.next:=ShedUU; ShedUU:=cur; {link to unused}
-   cur:=pcur^;
-  end else begin
-   pcur:=@cur^.next;
-   cur:=pcur^;
-  end;
- end;
-end;
+(**** Config File ****)
 
 var DoShowOpts:boolean=false;
-function OptIndex(o:string):word;
- begin
- if DoShowOpts then writeln('Option: ',o);
- result:=paramcount;
- while result>0 do begin
-  if o=system.paramstr(result) then break;
-  dec(result);
- end;
-end;
-
-function OptParamCount(o:word):word;
- var i:word;
- begin
- result:=0;
- if o>0 then for i:=o+1 to paramcount do begin
-  if paramstr(i)[1]<>'-' then inc(result)
-  else break;
- end;
-end;
 
 var ConfigFile:tConfigFile;
 var ConfigRefs:word;
+var Config:tConfigSection;
+
 procedure InitConfig;
   var fs:tFileStream;
   var fn:ansistring;
@@ -447,6 +274,7 @@ procedure InitConfig;
     raise eInOutError.Create('ServerLoop: Error changing working directory');
   writeln('ServerLoop:',' cfg=',fn,' pwd=',GetCurrentDir(),' pid=',GetProcessID);
 end;
+
 procedure AcquireConfigSection(out sect:tConfigSection; name:pchar);
   begin
   assert(ConfigRefs>0);
@@ -483,7 +311,156 @@ function GetCfgNum(name:pchar):Double;
   end end;
 end;
 
-//var nb:array [0..0] of byte;
+function OptIndex(o:string):word;
+ begin
+ if DoShowOpts then writeln('Option: ',o);
+ result:=paramcount;
+ while result>0 do begin
+  if o=system.paramstr(result) then break;
+  dec(result);
+ end;
+end;
+function OptParamCount(o:word):word;
+ var i:word;
+ begin
+ result:=0;
+ if o>0 then for i:=o+1 to paramcount do begin
+  if paramstr(i)[1]<>'-' then inc(result)
+  else break;
+ end;
+end;
+
+(**** Polled Descriptors Registry ****)
+
+type tPollTop=0..7;
+var pollArr: packed array [tPollTop] of tPollFd;
+type tFdHndDsc=record
+ cb: tFDEventHandler; {proc+object}
+ end;
+var pollHnd: array [tPollTop] of tFdHndDsc;
+var pollTop: tPollTop;
+
+procedure WatchFD(fd:tHandle; h:tFDEventHandler; e:LongWord);
+ var opt: tPollTop;
+begin
+ if assigned(h) then begin
+  PollHnd[pollTop].CB:=h;
+  PollArr[pollTop].fd:=fd;
+  PollArr[pollTop].events:=e;
+  PollArr[pollTop].revents:=0;
+  //writeln('Add watch ',pollTop,' on ',fd,' to ',IntToHex(qword(@h),8));
+  Inc(PollTop);
+ end else for opt:=0 to high(opt) do if PollArr[opt].fd=fd then begin
+  if (pollTop-1)>opt then begin
+   PollArr[opt]:=PollArr[pollTop-1];
+   PollHnd[opt]:=PollHnd[pollTop-1];
+  end;
+  dec(pollTop);
+  PollArr[pollTop].fd:=-1;
+  PollArr[pollTop].events:=0;
+  PollArr[pollTop].revents:=0;
+  break;
+ end;
+end;
+procedure WatchFD(fd:tHandle; h:tFDEventHandler);
+  begin
+  WatchFD(fd,h,POLLERR or POLLHUP or POLLIN);
+end;
+procedure WatchFDRW(fd:tHandle; h:tFDEventHandler);
+  begin
+  WatchFD(fd,h,POLLERR or POLLHUP or POLLIN or POLLOUT);
+end;
+
+procedure s_SetupInet; forward;
+
+procedure Main;
+ var tp:tPollTop;
+ var EventsCount:integer;
+ begin
+ s_setupInet;
+ ReleaseConfigSection(Config);
+ while not terminated  do begin
+  PollTimeout:=5000;
+  Timer;
+  Flush(OUTPUT);
+  EventsCount:=fpPoll(@PollArr[0],PollTop,PollTimeout);
+  Timer;
+  if (eventscount=-1)and terminated then break;
+  if eventscount=-1 then break;  {fixme: print error}
+  if eventscount=0 then continue else begin
+   {INET socket}
+   Reciever(PollArr[0]);
+   {INET6...}
+   {Generic}
+   for tp:=1 to pollTop do if PollArr[tp].revents>0 then begin
+    PollHnd[tp].CB(PollArr[tp].rEvents);
+    PollArr[tp].revents:=0;
+   end;
+  end;
+ end;
+ if assigned(onTerminate) then onTerminate;
+ CloseSocket(PollArr[0].fd);
+ if ReExec then fpExecv(paramstr(0),argv);
+end;
+
+(**** Sockets ****)
+var s_ip4: tSocket;
+
+function GetSocket(const rcpt:tNetAddr):tSocket;
+ begin
+ result:=s_ip4;
+end;
+procedure SendMessage(const data; len:word; const rcpt:tSockAddrL );
+ begin
+ {SC(@fpsendto,}fpsendto(s_ip4,@data,len,0,@rcpt,sizeof(sockaddr_in)){)};
+end;
+procedure SendMessage(const data; len:word; const rcpt:tNetAddr );
+ var sa:tSockAddrL;
+ begin
+ rcpt.ToSocket(sa);
+ SendMessage(data,len,sa);
+end;
+procedure SendMessage(const data; len:word; const rcpt:tNetAddr; channel:word );
+ begin
+ SendMessage(data,len,rcpt);
+ {todo: optimization??}
+end;
+
+procedure s_SetupInet;
+ var bind_addr:tInetSockAddr;
+ var turnon:cint;
+ begin
+  with bind_addr do begin
+   sin_family:=AF_INET;
+   sin_port:=htons(trunc(GetCfgNum('port')));
+   sin_addr.s_addr:=0; {any}
+   s_ip4:=fpSocket(sin_family,SOCK_DGRAM,IPPROTO_UDP);
+   SC(@fpSocket,s_ip4);
+   turnon:=IP_PMTUDISC_DO;
+   SC(@fpsetsockopt,fpsetsockopt(s_ip4, IPPROTO_IP, IP_MTU_DISCOVER, @turnon, sizeof(turnon)));
+  end;
+  SC(@fpBind,fpBind(s_ip4,@bind_addr,sizeof(bind_addr)));
+  with PollArr[0] do begin
+   fd:=s_ip4;
+   events:=pollIN;
+   revents:=0;
+  end;
+end;
+
+(**** Other ****)
+{$IFDEF Linux}
+function prctl( option:cint; arg2,arg3,arg4,arg5:culong):cint; cdecl; external;
+const PR_SET_NAME=15;
+procedure SetThreadName(name:pchar);
+begin prctl(PR_SET_NAME,culong(pchar(name)),0,0,0)
+end;
+{$ELSE}
+procedure SetThreadName(name:pchar);
+begin{$NOTE Custom thread mames not supported}
+end;
+{$ENDIF}
+
+(**** Unit Initialization ****)
 BEGIN
  writeln('ServerLoop: ','BrodNetD',' ',GIT_VERSION);
  if OptIndex('-h')>0 then DoShowOpts:=true;
@@ -499,7 +476,6 @@ BEGIN
  ShedTop:=nil;
  ShedUU:=nil; {todo: allocate a few to improve paging}
  InitMTime;
- LastShed:=GetMTime;
  OnTerminate:=nil;
  //SetTextBuf(OUTPUT,nb);
  Flush(OUTPUT);
